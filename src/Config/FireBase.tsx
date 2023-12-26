ab20eed4e9cd392dee28badc199ce7c7f4e65350
jesst <jessterman666@gmail.com>
2023-12-26 16:39:04 -0600

    Add Firebase configuration to package.json

---
+++ src/Config/FireBase.tsx
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD2x29tZfziJRPDlt1BQFChoFDQKuXfsOE",
+  authDomain: "studyapp-14658.firebaseapp.com",
+  databaseURL: "https://studyapp-14658-default-rtdb.firebaseio.com",
+  projectId: "studyapp-14658",
+  storageBucket: "studyapp-14658.appspot.com",
+  messagingSenderId: "101460716063",
+  appId: "1:101460716063:web:e86ee308a1e393cc90832a",
+  measurementId: "G-82S3JZKWJV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
