5cf020729174d931f99fc8209e1ec424b452ef6a
Mitchell Wagner <pim128@usask.ca>
2023-12-30 12:01:29 -0700

    Fixed empty TODO input

--- src/Components/Home/TodoList.tsx
+++ src/Components/Home/TodoList.tsx
@@ -23,113 +23,112 @@ import { Task } from '../../FireBaseManagement/AppBaseTypes';
 
 export default function TodoList() {
   const [checked, setChecked] = React.useState<number[]>([]);
-    const [todoList, setTodoList] = React.useState<Task[]>([]);
-    const [open, setOpen] = React.useState(false);
-    const [newTodo, setNewTodo] = React.useState('');
+  const [todoList, setTodoList] = React.useState<Task[]>([]);
+  const [open, setOpen] = React.useState(false);
+  const [newTodo, setNewTodo] = React.useState('');
 
-    const auth = getAuth();
+  const auth = getAuth();
 
-    React.useEffect(() => {
-        if (auth.currentUser !== null) {
-        const userRef = doc(db, 'users', auth.currentUser?.uid);
-        const unsubscribe = onSnapshot(userRef, (doc) => {
-            const data = doc.data();
-            if (data?.TodoList) {
-            setTodoList(data.TodoList);
-            }
-        });
-
-        return () => unsubscribe();
+  React.useEffect(() => {
+    if (auth.currentUser !== null) {
+      const userRef = doc(db, 'users', auth.currentUser?.uid);
+      const unsubscribe = onSnapshot(userRef, (doc) => {
+        const data = doc.data();
+        if (data?.TodoList) {
+          setTodoList(data.TodoList);
         }
-    }, []);
+      });
 
-    const handleToggle = (value: number) => () => {
-        const currentIndex = checked.indexOf(value);
-        const newChecked = [...checked];
+      return () => unsubscribe();
+    }
+  }, []);
 
-        if (currentIndex === -1) {
-        newChecked.push(value);
-        } else {
-        newChecked.splice(currentIndex, 1);
-        }
+  const handleToggle = (value: number) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
 
-        setChecked(newChecked);
-    };
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
 
-    const handleOpenDialog = () => {
-        setOpen(true);
-    };
+    setChecked(newChecked);
+  };
 
-    const handleCloseDialog = () => {
-        setOpen(false);
-    };
+  const handleOpenDialog = () => {
+    setOpen(true);
+  };
 
-    const handleAddTodo = async () => {
-        const newTask = { Name: newTodo, isComplete: false };
+  const handleCloseDialog = () => {
+    setOpen(false);
+  };
 
-        if (auth.currentUser !== null) {
-        const userRef = doc(db, 'users', auth.currentUser?.uid);
-        await updateDoc(userRef, {
-            TodoList: arrayUnion(newTask),
-        });
-        setNewTodo('');
-        handleCloseDialog();
-        }
-    };
+  const handleAddTodo = async () => {
+    const newTask = { Name: newTodo, isComplete: false };
 
+    if (auth.currentUser !== null) {
+      const userRef = doc(db, 'users', auth.currentUser?.uid);
+      await updateDoc(userRef, {
+        TodoList: arrayUnion(newTask),
+      });
+      setNewTodo('');
+      handleCloseDialog();
+    }
+  };
 
-    return (
-        <ThemeProvider theme={lighter}>
-        <Button onClick={handleOpenDialog}>Add TODO</Button>
-        <Dialog open={open} onClose={handleCloseDialog}>
-            {/* ... (previous dialog content) */}
-        </Dialog>
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: lighter.palette.secondary.main }}>
-            {todoList.map((task, index) => {
-            const labelId = `checkbox-list-label-${index}`;
+  return (
+    <ThemeProvider theme={lighter}>
+      <Button onClick={handleOpenDialog}>Add TODO</Button>
+      <Dialog open={open} onClose={handleCloseDialog}>
+        <DialogTitle>Add New TODO</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="Task Name"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog}>Cancel</Button>
+          <Button onClick={handleAddTodo}>Add</Button>
+        </DialogActions>
+      </Dialog>
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: lighter.palette.secondary.main }}>
+        {todoList.map((task, index) => {
+          const labelId = `checkbox-list-label-${index}`;
 
-            return (
-                <ListItem
-                key={index}
-                sx={{
-                    width: '100%',
-                    overflowX: 'auto',
-                }}
-                secondaryAction={
-                    <IconButton
-                    edge="end"
-                    aria-label="comments"
-                    style={{ backgroundColor: lighter.palette.primary.main, color: lighter.palette.primary.contrastText }}
-                    >
-                    <DeleteForeverIcon />
-                    </IconButton>
-                }
-                disablePadding
+          return (
+            <ListItem
+              key={index}
+              secondaryAction={
+                <IconButton
+                  edge="end"
+                  aria-label="comments"
+                  style={{ backgroundColor: lighter.palette.primary.main, color: lighter.palette.primary.contrastText }}
                 >
-                <ListItemButton role={undefined} onClick={handleToggle(index)} dense>
-                    <ListItemIcon>
-                    <Checkbox
-                        edge="start"
-                        checked={checked.indexOf(index) !== -1}
-                        tabIndex={-1}
-                        disableRipple
-                        inputProps={{ 'aria-labelledby': labelId }}
-                    />
-                    </ListItemIcon>
-                    <ListItemText
-                    id={labelId}
-                    primary={task.Name}
-                    sx={{
-                        color: lighter.palette.primary.main,
-                        whiteSpace: 'pre-wrap', // Enable line breaks for long text
-                        overflowWrap: 'break-word', // Enable word breaks for long words
-                    }}
-                    />
-                </ListItemButton>
-                </ListItem>
-            );
-            })}
-        </List>
-        </ThemeProvider>
-    );
+                  <DeleteForeverIcon />
+                </IconButton>
+              }
+              disablePadding
+            >
+              <ListItemButton role={undefined} onClick={handleToggle(index)} dense>
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={checked.indexOf(index) !== -1}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ 'aria-labelledby': labelId }}
+                  />
+                </ListItemIcon>
+                <ListItemText id={labelId} primary={task.Name} sx={{ color: lighter.palette.primary.main }} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
+    </ThemeProvider>
+  );
 }
