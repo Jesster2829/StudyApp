b79a0e845b96006f0be70b6f9275e41ba11ed182
Mitchell Wagner <pim128@usask.ca>
2023-12-29 21:44:47 -0700

    Added basic TODO list

--- src/Components/Home/TodoList.tsx
+++ src/Components/Home/TodoList.tsx
@@ -6,29 +6,35 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-import CommentIcon from '@mui/icons-material/Comment';
-import { ThemeProvider } from '@mui/material';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { Button, ThemeProvider } from '@mui/material';
 import { lighter } from '../../themes';
 import { doc, onSnapshot } from "firebase/firestore";
 import { db } from '../../Config/FireBase';
-
+import { getAuth } from 'firebase/auth';
+import { arrayUnion, updateDoc } from "firebase/firestore";
+import { Task } from '../../FireBaseManagement/AppBaseTypes';
 
 export default function TodoList() {
     const [checked, setChecked] = React.useState([0]);
-    const [todoList, setTodoList] = React.useState([]);
+    const [todoList, setTodoList] = React.useState<Task[]>([]);
+
+    const auth = getAuth();
 
     React.useEffect(() => {
-        const userRef = doc(db, 'users', 'userId'); // replace 'userId' with the actual user ID
-        const unsubscribe = onSnapshot(userRef, (doc) => {
-            const data = doc.data();
-            if (data?.TodoList) {
-                setTodoList(data.TodoList);
-                console.log(data.TodoList);
-            }
-        });
 
-        // Cleanup function to unsubscribe from the snapshot listener when the component unmounts
-        return () => unsubscribe();
+        if (auth.currentUser !== null) {
+            const userRef = doc(db, 'users', auth.currentUser?.uid);
+            const unsubscribe = onSnapshot(userRef, (doc) => {
+                const data = doc.data();
+                if (data?.TodoList) {
+                    setTodoList(data.TodoList);
+                    console.log(todoList);
+                }
+            });
+    
+            return () => unsubscribe();
+        }
     }, []);
 
     const handleToggle = (value: number) => () => {
@@ -44,38 +50,54 @@ export default function TodoList() {
         setChecked(newChecked);
     };
 
+    const addTodo = async () => {
+        const newTask = { Name: 'Testing', isComplete: false }; // The new task to add
+    
+        if (auth.currentUser !== null) {
+            const userRef = doc(db, 'users', auth.currentUser?.uid);
+            await updateDoc(userRef, {
+                TodoList: arrayUnion(newTask)
+            });
+        }
+    }
+
     return (
         <ThemeProvider theme={lighter}>
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: lighter.palette.secondary.main }}>
-        {[0, 1, 2, 3].map((value) => {
-            const labelId = `checkbox-list-label-${value}`;
-
-            return (
-            <ListItem
-                key={value}
-                secondaryAction={
-                <IconButton edge="end" aria-label="comments" style={{ backgroundColor: lighter.palette.primary.main, color: lighter.palette.primary.contrastText }}>
-                    <CommentIcon />
-                </IconButton>
-                }
-                disablePadding
+            <Button
+            onClick={addTodo}
             >
-                <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
-                <ListItemIcon>
-                    <Checkbox
-                    edge="start"
-                    checked={checked.indexOf(value) !== -1}
-                    tabIndex={-1}
-                    disableRipple
-                    inputProps={{ 'aria-labelledby': labelId }}
-                    />
-                </ListItemIcon>
-                <ListItemText id={labelId} primary={`Line item ${value + 1}`} sx={{ color: lighter.palette.primary.main }} />
-                </ListItemButton>
-            </ListItem>
-            );
-        })}
-        </List>
+                Add TODO
+            </Button>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: lighter.palette.secondary.main }}>
+    {todoList.map((task, index) => {
+        const labelId = `checkbox-list-label-${index}`;
+
+        return (
+        <ListItem
+            key={index}
+            secondaryAction={
+            <IconButton edge="end" aria-label="comments" style={{ backgroundColor: lighter.palette.primary.main, color: lighter.palette.primary.contrastText }}>
+                <DeleteForeverIcon />
+            </IconButton>
+            }
+            disablePadding
+        >
+            <ListItemButton role={undefined} onClick={handleToggle(index)} dense>
+            <ListItemIcon>
+                <Checkbox
+                edge="start"
+                checked={checked.indexOf(index) !== -1}
+                tabIndex={-1}
+                disableRipple
+                inputProps={{ 'aria-labelledby': labelId }}
+                />
+            </ListItemIcon>
+            <ListItemText id={labelId} primary={task.Name} sx={{ color: lighter.palette.primary.main }} />
+            </ListItemButton>
+        </ListItem>
+        );
+    })}
+</List>
         </ThemeProvider>
     );
 }
