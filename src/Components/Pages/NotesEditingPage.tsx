701bc5a85b5cba812260fb545a4e8355f207268e
Madison <malab@shaw.ca>
2024-01-07 17:45:31 -0600

    note editing page shows content of the note being edited

--- src/Components/Pages/NotesEditingPage.tsx
+++ src/Components/Pages/NotesEditingPage.tsx
@@ -3,28 +3,47 @@ import React from "react";
 import { darker } from "../../themes";
 import { Button, Paper, TextField, Typography } from "@mui/material";
 import ResponsiveAppBar from "../PageHeaders/homeHeader";
+import { Note } from "../../FireBaseManagement/AppBaseTypes";
+import { useLocation } from "react-router-dom";
 
 export const NoteEditing = () => {
+  const [notes, setNotes] = React.useState<Note[]>([{Name: "Math", Content: "math"}, {Name: "Astronomy", Content: "astronomy"}]);
+  const [content, setContent] = React.useState<String>("");
+
+  const location = useLocation();
+  const noteName = location.state?.NoteName || "";
+
+  React.useEffect(() => {
+    notes.forEach((n) => {
+      if (n.Name === noteName) {
+        setContent(n.Content);
+      }
+    });
+  }, []);
+
   return (
     <ThemeProvider theme={darker}>
       <ResponsiveAppBar />
+      <br></br>
       <Paper 
         sx={{
           p: 2,
           display: 'flex',
           flexDirection: 'column',
-          height: 500,
+          // height: 500,
           // figure out the width
         }}>
         
-        <TextField
-          defaultValue="hello" 
+        {/* figure out the white space and resizing */}
+        {/* <TextField
+          defaultValue="hello"
           multiline
           fullWidth
           sx={{
-            whiteSpace: 'pre-line',
+            // whiteSpace: 'pre-line',
             '& fieldset': {border: 'none'},
-          }}/>
+          }}/> */}
+          <Typography>{content}</Typography>
       </Paper>
       <Button>Save</Button>
     </ThemeProvider>
--- src/Components/Pages/NotesPage.tsx
+++ src/Components/Pages/NotesPage.tsx
@@ -2,15 +2,22 @@ import { Button, Paper, Stack, ThemeProvider, Typography } from "@mui/material";
 import React from "react";
 import { darker } from "../../themes";
 import ResponsiveAppBar from "../PageHeaders/homeHeader";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Note } from "../../FireBaseManagement/AppBaseTypes";
 
 export const Notes = () => {
     const [notes, setNotes] = React.useState<Note[]>([]);
 
+    const navigate = useNavigate();
+
     React.useEffect(() => {
-        setNotes([{Name: "Math", Content: ""}, {Name: "Astronomy", Content: ""}]);
+        setNotes([{Name: "Math", Content: "math"}, {Name: "Astronomy", Content: "astronomy"}]);
     }, []);
+
+    function selectNote(name: string) {
+        console.log(name);
+        navigate(`/notes`, {state: {NoteName: name}});
+    }
 
     return (
         <ThemeProvider theme={darker}>
@@ -18,12 +25,15 @@ export const Notes = () => {
 
             <Stack spacing={1}>
             {notes.map((note) => (
-                <Paper sx={{ p: 2 }}>
+                <Paper 
+                  key={note.Name}
+                  sx={{ p: 2 }}>
                     <Stack direction="row" alignItems="center" justifyContent="space-between">
                         <Typography>{note.Name}</Typography>
-                        <Link to="/notes">
-                            <Button>Edit</Button>
-                        </Link>
+                            <Button
+                            onClick={() => {
+                                console.log(note.Name);
+                                selectNote(note.Name)}}>Edit</Button>
                     </Stack>
                 </Paper>
             ))}
