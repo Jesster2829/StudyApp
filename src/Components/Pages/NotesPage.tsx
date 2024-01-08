8003830dc4693d8844db88690a80e16ccaa76a9a
Madison <malab@shaw.ca>
2024-01-07 21:27:52 -0600

    can now create notes

--- src/Components/Pages/NotesPage.tsx
+++ src/Components/Pages/NotesPage.tsx
@@ -1,4 +1,4 @@
-import { Button, Paper, Stack, ThemeProvider, Typography } from "@mui/material";
+import { Button, Dialog, DialogContent, Paper, Stack, TextField, ThemeProvider, Typography } from "@mui/material";
 import React from "react";
 import { darker } from "../../themes";
 import ResponsiveAppBar from "../PageHeaders/homeHeader";
@@ -7,6 +7,8 @@ import { Note } from "../../FireBaseManagement/AppBaseTypes";
 
 export const Notes = () => {
     const [notes, setNotes] = React.useState<Note[]>([]);
+    const [open, setOpen] = React.useState(false);
+    const [newNote, setNewNote] = React.useState<string>("");
 
     const navigate = useNavigate();
 
@@ -17,6 +19,24 @@ export const Notes = () => {
     function selectNote(name: string) {
         console.log(name);
         navigate(`/notes`, {state: {NoteName: name}});
+    }
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+    
+    const handleCreate = () => {
+        console.log("create");
+        setNotes([...notes, {Name: newNote, Content: ""}]);
+        setOpen(false);
+    }
+
+    const handleNoteNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNewNote(event.target.value);
     }
 
     return (
@@ -30,14 +50,28 @@ export const Notes = () => {
                   sx={{ p: 2 }}>
                     <Stack direction="row" alignItems="center" justifyContent="space-between">
                         <Typography>{note.Name}</Typography>
-                            <Button
-                            onClick={() => {
-                                console.log(note.Name);
-                                selectNote(note.Name)}}>Edit</Button>
+                        <Button
+                          onClick={() => selectNote(note.Name)}    
+                        >Edit</Button>
                     </Stack>
                 </Paper>
             ))}
+            <Button onClick={handleOpen}>
+                New Note
+            </Button>
             </Stack>
+            
+            <Dialog onClose={handleClose} open={open}>
+                <DialogContent>
+                    <TextField 
+                      fullWidth
+                      id="Note Name"
+                      onChange={handleNoteNameChange}
+                    />
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleCreate}>Create</Button>
+                </DialogContent>
+            </Dialog>
 
         </ThemeProvider>
     )
